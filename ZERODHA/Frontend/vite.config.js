ae6a9142045debbe3109a1535e71ce06f4796aa3
Dilkhush Kumar <dk5847001@gmail.com>
2026-09-07 11:07:12 +0530

    frontend setup completed

--- ZERODHA/Frontend/vite.config.js
+++ ZERODHA/Frontend/vite.config.js
@@ -1,7 +1,25 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, transformWithOxc } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+const transformJsxInJs = () => ({
+    name: "transform-jsx-in-js",
+
+    enforce: "pre",
+
+    async transform(code, id) {
+        if (!id.endsWith(".js")) {
+            return null;
+        }
+
+        return await transformWithOxc(code, id, {
+            lang: "jsx",
+        });
+    },
+});
+
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(),
+        transformJsxInJs(),
+    ],
+});
