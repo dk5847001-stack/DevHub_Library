e378bafc08591f78117ce907c0c74e6bdcab230b
Dilkhush Kumar <dk5847001@gmail.com>
2026-09-10 11:55:40 +0530

    CreateTicket completedd

--- ZERODHA/Frontend/src/components/Support/CreateTicket.js
+++ ZERODHA/Frontend/src/components/Support/CreateTicket.js
@@ -1,7 +1,115 @@
-export default function CreateTicket(){
-    return(
+export default function CreateTicket() {
+    return (
         <div>
-            CreateTicket
+            <div className="flex justify-around">
+                <div className="flex flex-col">
+                    <p className="text-2xl text-gray-800 font-semibold my-8">To create a ticket, select a relevant topic</p>
+                    <p className="flex gap-2 font-semibold mb-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                            <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z" clipRule="evenodd" />
+                        </svg>
+
+                        Account Opening
+                    </p>
+                    <p className="text-blue-400 mb-3 text-sm">Online Account Opening</p>
+                    <p className="text-blue-400 mb-3 text-sm">Offline Account Opening</p>
+                    <p className="text-blue-400 mb-3 text-sm">Company, Partnership and HUF Account opening</p>
+                    <p className="text-blue-400 mb-3 text-sm">NRI Accont Opening</p>
+                    <p className="text-blue-400 mb-3 text-sm">Charges at Zerodha</p>
+                    <p className="text-blue-400 mb-3 text-sm">Zerodha IDFC FIRST Bank 3-in-1 Account</p>
+                    <p className="text-blue-400 mb-3 text-sm">Getting Started</p>
+                </div>
+                {/* ------------------------- */}
+                <div className="flex mt-24 flex-col">
+                    <p className="flex gap-2 font-semibold mb-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                            <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clip-rule="evenodd" />
+                        </svg>
+
+                        Your Zerodha Account
+                    </p>
+                    <p className="text-blue-400 mb-3 text-sm">Margin/leverage, Product and Order types</p>
+                    <p className="text-blue-400 mb-3 text-sm">Kite Web and Mobile</p>
+                    <p className="text-blue-400 mb-3 text-sm">Trading FAQs</p>
+                    <p className="text-blue-400 mb-3 text-sm">Corporate Actions</p>
+                    <p className="text-blue-400 mb-3 text-sm">Sentinel</p>
+                    <p className="text-blue-400 mb-3 text-sm">Kite API</p>
+                    <p className="text-blue-400 mb-3 text-sm">Pi and other platforms</p>
+                    <p className="text-blue-400 mb-3 text-sm">Stockerports+</p>
+                    <p className="text-blue-400 mb-3 text-sm">GTT</p>
+                </div>
+                {/* ------------------------------ */}
+                <div className="flex mt-24 flex-col">
+                    <p className="flex gap-2 font-semibold mb-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                            <path d="M18.75 12.75h1.5a.75.75 0 0 0 0-1.5h-1.5a.75.75 0 0 0 0 1.5ZM12 6a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5A.75.75 0 0 1 12 6ZM12 18a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5A.75.75 0 0 1 12 18ZM3.75 6.75h1.5a.75.75 0 1 0 0-1.5h-1.5a.75.75 0 0 0 0 1.5ZM5.25 18.75h-1.5a.75.75 0 0 1 0-1.5h1.5a.75.75 0 0 1 0 1.5ZM3 12a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5A.75.75 0 0 1 3 12ZM9 3.75a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM12.75 12a2.25 2.25 0 1 1 4.5 0 2.25 2.25 0 0 1-4.5 0ZM9 15.75a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5Z" />
+                        </svg>
+
+
+                        Account Opening
+                    </p>
+                    <p className="text-blue-400 mb-3 text-sm">Login Credentials</p>
+                    <p className="text-blue-400 mb-3 text-sm">Account Modification and Segment Addition</p>
+                    <p className="text-blue-400 mb-3 text-sm">DP ID and bank details</p>
+                    <p className="text-blue-400 mb-3 text-sm">YOur Profile</p>
+                    <p className="text-blue-400 mb-3 text-sm">Transfer and conversion of shares</p>
+                </div>
+            </div>
+            {/* part 2-------------------------------------------------- */}
+            <div className="flex justify-around">
+                <div className="flex mt-24 flex-col">
+                    <p className="flex gap-2 font-semibold mb-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                            <path d="M4.5 3.75a3 3 0 0 0-3 3v.75h21v-.75a3 3 0 0 0-3-3h-15Z" />
+                            <path fillRule="evenodd" d="M22.5 9.75h-21v7.5a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3v-7.5Zm-18 3.75a.75.75 0 0 1 .75-.75h6a.75.75 0 0 1 0 1.5h-6a.75.75 0 0 1-.75-.75Zm.75 2.25a.75.75 0 0 0 0 1.5h3a.75.75 0 0 0 0-1.5h-3Z" clipRule="evenodd" />
+                        </svg>
+
+
+                        Funds
+                    </p>
+                    <p className="text-blue-400 mb-3 text-sm">Adding Funds</p>
+                    <p className="text-blue-400 mb-3 text-sm">Fund Withdrawal</p>
+                    <p className="text-blue-400 mb-3 text-sm">eMandates</p>
+                    <p className="text-blue-400 mb-3 text-sm">Adding Bank Accounts</p>
+                </div>
+                {/* ------------------------- */}
+                <div className="flex mt-24 flex-col">
+                    <p className="flex gap-2 font-semibold mb-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                            <path fill-rule="evenodd" d="M2.25 6a3 3 0 0 1 3-3h13.5a3 3 0 0 1 3 3v12a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V6Zm3.97.97a.75.75 0 0 1 1.06 0l2.25 2.25a.75.75 0 0 1 0 1.06l-2.25 2.25a.75.75 0 0 1-1.06-1.06l1.72-1.72-1.72-1.72a.75.75 0 0 1 0-1.06Zm4.28 4.28a.75.75 0 0 0 0 1.5h3a.75.75 0 0 0 0-1.5h-3Z" clip-rule="evenodd" />
+                        </svg>
+
+                        Console
+                    </p>
+                    <p className="text-blue-400 mb-3 text-sm">Reports</p>
+                    <p className="text-blue-400 mb-3 text-sm">Ledger</p>
+                    <p className="text-blue-400 mb-3 text-sm">Portfolio</p>
+                    <p className="text-blue-400 mb-3 text-sm">60 Day Challenge</p>
+                    <p className="text-blue-400 mb-3 text-sm">IPO</p>
+                    <p className="text-blue-400 mb-3 text-sm">rEFERRAL pROGRAM </p>
+                    <p className="text-blue-400 mb-3 text-sm">Pi and other platforms</p>
+                    <p className="text-blue-400 mb-3 text-sm">Stockerports+</p>
+                    <p className="text-blue-400 mb-3 text-sm">GTT</p>
+                </div>
+                {/* ------------------------------ */}
+                <div className="flex mt-24 flex-col">
+                    <p className="flex gap-2 font-semibold mb-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                            <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clipRule="evenodd" />
+                        </svg>
+
+                        Coin
+                    </p>
+                    <p className="text-blue-400 mb-3 text-sm">Understanding Mutual</p>
+                    <p className="text-blue-400 mb-3 text-sm">About Coin</p>
+                    <p className="text-blue-400 mb-3 text-sm">Buying and Selling through Coin</p>
+                    <p className="text-blue-400 mb-3 text-sm">Starting an SIP</p>
+                    <p className="text-blue-400 mb-3 text-sm">Managing your Portfolio</p>
+                    <p className="text-blue-400 mb-3 text-sm">Coin App</p>
+                    <p className="text-blue-400 mb-3 text-sm">Moving to Coin</p>
+                    <p className="text-blue-400 mb-3 text-sm">Government Securities</p>
+                </div>
+            </div>
         </div>
     )
 }
